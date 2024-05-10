c1a046283d10439520f6267573fb61e2d535990f
felix <felixdittrich92@gmail.com>
2024-05-10 06:10:03 +0200

    remove unused vis utils

--- onnxtr/utils/visualization.py
+++ onnxtr/utils/visualization.py
@@ -2,7 +2,7 @@
 
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
-import colorsys
+
 from copy import deepcopy
 from typing import Any, Dict, List, Optional, Tuple, Union
 
@@ -14,7 +14,7 @@ from matplotlib.figure import Figure
 
 from .common_types import BoundingBox, Polygon4P
 
-__all__ = ["visualize_page", "visualize_kie_page", "draw_boxes"]
+__all__ = ["visualize_page", "draw_boxes"]
 
 
 def rect_patch(
@@ -140,26 +140,6 @@ def create_obj_patch(
     raise ValueError("invalid geometry format")
 
 
-def get_colors(num_colors: int) -> List[Tuple[float, float, float]]:
-    """Generate num_colors color for matplotlib
-
-    Args:
-    ----
-        num_colors: number of colors to generate
-
-    Returns:
-    -------
-        colors: list of generated colors
-    """
-    colors = []
-    for i in np.arange(0.0, 360.0, 360.0 / num_colors):
-        hue = i / 360.0
-        lightness = (50 + np.random.rand() * 10) / 100.0
-        saturation = (90 + np.random.rand() * 10) / 100.0
-        colors.append(colorsys.hls_to_rgb(hue, lightness, saturation))
-    return colors
-
-
 def visualize_page(
     page: Dict[str, Any],
     image: np.ndarray,
@@ -274,83 +254,6 @@ def visualize_page(
                 )
                 ax.add_patch(rect)
                 if interactive:
-                    artists.append(rect)
-
-    if interactive:
-        import mplcursors
-
-        # Create mlp Cursor to hover patches in artists
-        mplcursors.Cursor(artists, hover=2).connect("add", lambda sel: sel.annotation.set_text(sel.artist.get_label()))
-    fig.tight_layout(pad=0.0)
-
-    return fig
-
-
-def visualize_kie_page(
-    page: Dict[str, Any],
-    image: np.ndarray,
-    words_only: bool = False,
-    display_artefacts: bool = True,
-    scale: float = 10,
-    interactive: bool = True,
-    add_labels: bool = True,
-    **kwargs: Any,
-) -> Figure:
-    """Visualize a full page with predicted blocks, lines and words
-
-    >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
-    >>> from onnxtr.utils.visualization import visualize_page
-    >>> from onnxtr.models import ocr_db_crnn
-    >>> model = ocr_db_crnn()
-    >>> input_page = (255 * np.random.rand(600, 800, 3)).astype(np.uint8)
-    >>> out = model([[input_page]])
-    >>> visualize_kie_page(out[0].pages[0].export(), input_page)
-    >>> plt.show()
-
-    Args:
-    ----
-        page: the exported Page of a Document
-        image: np array of the page, needs to have the same shape than page['dimensions']
-        words_only: whether only words should be displayed
-        display_artefacts: whether artefacts should be displayed
-        scale: figsize of the largest windows side
-        interactive: whether the plot should be interactive
-        add_labels: for static plot, adds text labels on top of bounding box
-        **kwargs: keyword arguments for the polygon patch
-
-    Returns:
-    -------
-        the matplotlib figure
-    """
-    # Get proper scale and aspect ratio
-    h, w = image.shape[:2]
-    size = (scale * w / h, scale) if h > w else (scale, h / w * scale)
-    fig, ax = plt.subplots(figsize=size)
-    # Display the image
-    ax.imshow(image)
-    # hide both axis
-    ax.axis("off")
-
-    if interactive:
-        artists: List[patches.Patch] = []  # instantiate an empty list of patches (to be drawn on the page)
-
-    colors = {k: color for color, k in zip(get_colors(len(page["predictions"])), page["predictions"])}
-    for key, value in page["predictions"].items():
-        for prediction in value:
-            if not words_only:
-                rect = create_obj_patch(
-                    prediction["geometry"],
-                    page["dimensions"],
-                    label=f"{key} \n {prediction['value']} (confidence: {prediction['confidence']:.2%}",
-                    color=colors[key],
-                    linewidth=1,
-                    **kwargs,
-                )
-                # add patch on figure
-                ax.add_patch(rect)
-                if interactive:
-                    # add patch to cursor's artists
                     artists.append(rect)
 
     if interactive:
