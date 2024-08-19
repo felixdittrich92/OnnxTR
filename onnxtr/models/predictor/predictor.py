ecdb1b6dfecba5d20481401a7078ada10ba4cb6f
Felix Dittrich <felixdittrich92@gmail.com>
2024-08-19 10:01:02 +0200

    [Fix] fix shapes with straighten pages (#28)

--- onnxtr/models/predictor/predictor.py
+++ onnxtr/models/predictor/predictor.py
@@ -100,6 +100,8 @@ class OCRPredictor(NestedObject, _OCRPredictor):
             origin_pages_orientations = None
         if self.straighten_pages:
             pages = self._straighten_pages(pages, seg_maps, general_pages_orientations, origin_pages_orientations)
+            # update page shapes after straightening
+            origin_page_shapes = [page.shape[:2] for page in pages]
 
             # forward again to get predictions on straight pages
             loc_preds = self.det_predictor(pages, **kwargs)  # type: ignore[assignment]
