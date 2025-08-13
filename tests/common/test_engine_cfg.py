96f8c687ea5288f81d2a7eb88ac8cec5230f82bb
Florian Spieß <florian.spiess@cronn.de>
2025-08-13 10:10:24 +0200

    [Feat] Add run_options_provider for EngineConfig (#87)

--- tests/common/test_engine_cfg.py
+++ tests/common/test_engine_cfg.py
@@ -1,11 +1,20 @@
+import gc
+
 import numpy as np
+import psutil
 import pytest
-from onnxruntime import SessionOptions
+from onnxruntime import RunOptions, SessionOptions
 
 from onnxtr import models
 from onnxtr.io import Document
 from onnxtr.models import EngineConfig, detection, recognition
 from onnxtr.models.predictor import OCRPredictor
+
+
+def _get_rss_mb():
+    gc.collect()
+    process = psutil.Process()
+    return process.memory_info().rss / (1024 * 1024)
 
 
 def _test_predictor(predictor):
@@ -72,3 +81,54 @@ def test_engine_cfg(det_arch, reco_arch):
     reco_predictor = models.recognition_predictor(reco_arch, engine_cfg=engine_cfg)
     assert reco_predictor.model.providers == ["CPUExecutionProvider"]
     assert not reco_predictor.model.session_options.enable_cpu_mem_arena
+
+
+def test_cpu_memory_arena_shrinkage_enabled():
+    session_options = SessionOptions()
+    session_options.enable_mem_pattern = False
+    session_options.enable_cpu_mem_arena = True
+
+    enable_shrinkage = False
+
+    providers = [("CPUExecutionProvider", {"arena_extend_strategy": "kSameAsRequested"})]
+
+    def enable_arena_shrinkage(run_options: "RunOptions") -> "RunOptions":
+        if enable_shrinkage:
+            run_options.add_run_config_entry("memory.enable_memory_arena_shrinkage", "cpu:0")
+            assert run_options.get_run_config_entry("memory.enable_memory_arena_shrinkage") == "cpu:0"
+        return run_options
+
+    engine_cfg = EngineConfig(
+        providers=providers,
+        session_options=session_options,
+        run_options_provider=enable_arena_shrinkage,
+    )
+
+    predictor = models.ocr_predictor(
+        det_engine_cfg=engine_cfg,
+        reco_engine_cfg=engine_cfg,
+        clf_engine_cfg=engine_cfg,
+        detect_orientation=True,
+    )
+
+    assert predictor.det_predictor.model.providers == providers
+    assert predictor.det_predictor.model.session_options.enable_cpu_mem_arena
+    assert predictor.reco_predictor.model.providers == providers
+    assert predictor.reco_predictor.model.session_options.enable_cpu_mem_arena
+
+    rng = np.random.RandomState(seed=42)
+    sample = rng.randint(0, 256, (1024, 1024, 3), dtype=np.uint8)
+
+    start_rss = _get_rss_mb()
+
+    predictor([sample])
+    increased_rss = _get_rss_mb()
+
+    assert increased_rss > start_rss
+
+    enable_shrinkage = True
+
+    predictor([sample])
+    decreased_rss = _get_rss_mb()
+
+    assert increased_rss > decreased_rss
